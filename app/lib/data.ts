96c49499beaa7c660ea1be58e512fb0ad14db454
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-08-18 04:55:19 +0530

    PPR, Pagination & Errors
    adding next js 14 - new feature PPR,
    and also using error.jsx

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -84,7 +84,7 @@ export async function fetchCardData() {
   }
 }
 
-const ITEMS_PER_PAGE = 6;
+const ITEMS_PER_PAGE = 5;
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
@@ -159,6 +159,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
