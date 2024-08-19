b5db8d30303eec477dddf8038c7dab774c292984
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-08-19 20:46:01 +0530

    Resolve Error
    adding missing part
    deleteInvoice in action.ts

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -94,3 +94,17 @@ export async function updateInvoice(id: string, formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
+
+// delete invoice
+export async function deleteInvoice(id: string) {
+    throw new Error('Failed to Delete Invoice');
+
+    // Unreachable code block
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return { message: 'Deleted Invoice.' };
+    } catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+    }
+}
